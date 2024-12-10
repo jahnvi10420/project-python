94fb7d101319cc1b1506face356400459fdf9fa7
jahnvi10420 <jahnvipandey8858@gmail.com>
2024-12-10 22:34:27 +0530

    second commit

---
+++ test-1.py
@@ -0,0 +1,19 @@
+"""a = 5 + 3
+print(a)
+
+b = 5
+c = 6
+d = b + c
+print(d) 
+
+print(d + a)"""
+
+
+"""num = int(input("Tell me: "))
+
+if num % 2 == 0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")"""
+
+
